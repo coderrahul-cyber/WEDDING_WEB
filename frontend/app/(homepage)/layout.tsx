2d74f9fca6c76dbf3583705a4cccb49a86f38a7e
coderrahul-cyber <rahulsmant299@gmail.com>
2025-11-17 12:14:36 +0530

    Done

--- frontend/app/(homepage)/layout.tsx
+++ frontend/app/(homepage)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import styles from './globals.css';
+import   "../";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
