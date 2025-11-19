6b92ec944bf7dd4825a6e47f026e32296992ac20
coderrahul-cyber <rahulsmant299@gmail.com>
2025-11-19 10:44:57 +0530

    basic

--- frontend/app/(homepage)/page.tsx
+++ frontend/app/(homepage)/page.tsx
@@ -7,11 +7,6 @@ import Footer from '@/components/Footer';
 
 
 
-/**
- * Set the video URL.
- * Since your video is in the /public folder at the root of your frontend project
- * (e.g., /public/bg-video.mp4), you can access it directly with "/bg-video.mp4".
- */
 const VIDEO_URL = "/assets/video/web2.mov";
 
 
@@ -63,16 +58,16 @@ const App: React.FC = () => {
               <h1 className="text-4xl font-primary leading-none tracking-tight md:text-9xl">
                 STUDIO
                 <br />
-                NAME<sup className='text-xs md:text-xs  border-2 pr-1 rounded-full font-mono  ml-1'>TM</sup>
+                NAME
               </h1>
             </div>
             
             {/* Nav Links */}
-            <ul className="flex items-center gap-6 md:gap-10">
+            <ul className="flex items-center md:text-2xl font-primary-medium gap-6 md:gap-10">
               <li>
                 <Link
                   href="/work" 
-                  className="text-sm font-semibold tracking-wider transition-opacity hover:opacity-75 md:text-base"
+                  className=" tracking-wider transition-colors  hover:text-bg "
                 >
                   WORK
                 </Link>
@@ -81,18 +76,26 @@ const App: React.FC = () => {
                 {/* Updated this to a button to open the overlay */}
                 <button 
                   onClick={() => setIsContactOpen(true)}
-                  className="text-sm font-semibold tracking-wider transition-opacity hover:opacity-75 md:text-base"
+                  className="  tracking-wider transition-colors  hover:text-bg "
                 >
                   CONTACT
                 </button>
               </li>
               <li>
-                <a 
-                  href="#" 
-                  className="text-sm font-semibold tracking-wider transition-opacity hover:opacity-75 md:text-base"
+                <Link 
+                  href="https://www.instagram.com/jp___photography_"
+                  target='#' 
+                  className="  tracking-wider transition-colors  hover:text-bg "
                 >
-                  IG.YT
-                </a>
+                  IG
+                </Link>
+                .
+                 <Link 
+                  href="#" 
+                  className="  tracking-wider transition-colors  hover:text-bg "
+                >
+                  YT
+                </Link>
               </li>
             </ul>
           </nav>
@@ -100,12 +103,11 @@ const App: React.FC = () => {
           {/* Main Content (Bottom Left) */}
           <main className="mb-10">
             <h2 className="text-5xl font-primary md:text-7xl">Born to Create</h2>
-            <p className="mt-4 text-xl font-primary-medium tracking-wide md:text-2xl">
-              Brand.Design.Development
+            <p className="mt-4 text-xl font-primary-medium  md:text-2xl">
+              Lights.Camera.Action
             </p>
             <p className="md:mt-3 max-w-md  font-secondary  text-neutral-300 text-lg md:text-xl">
-              A Bootstrap startup for photography Studio Name is the best in the
-              area, cinematic, wedding
+             Every wedding has a script written by destiny. Let [Studio Name] capture the unscripted beauty of yours with our signature cinematic style.
             </p>
           </main>
         </div>
